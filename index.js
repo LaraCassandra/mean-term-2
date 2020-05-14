abda8d7e4872fe12f1f04010e28685e585bd1d6c
Lara Cook <190218@virtualwindow.co.za>
2020-05-14 17:00:36 +0200

    add contacts.html and chat.html, refactor into html, add simple router

--- index.js
+++ index.js
@@ -1,23 +1,87 @@
 
-var http = require('http');
-var server = http.createServer();
-var querystring = require('querystring');
-var fs =  require('fs');
+const http = require('http')
+const queryString = require('querystring')
+const server = http.createServer()
+var fs =  require('fs')
+const template = require('es6-template-strings')
+var contacts = []
 
-var handleFormGet = function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('templates/form.html', 'utf8', function(err, data) {
+var simpleRouter = (request) => {
+  var method = request.method; 
+  var path = request.url;
+  var suppliedRoute = {method: method, path: path}
+
+  var queryIndex = request.url.indexOf('?');
+  if (queryIndex >= 0){
+    path = request.url.slice(0, queryIndex)
+  }
+
+  var routes = [
+    {method: 'GET', path: '/', handler: handleFormGet},
+    {method: 'POST', path: '/', handler: handleFormPost}
+  ];
+  for (let i = 0; i < routes.length; i++){
+    var route = routes[i]
+    if(route.method === suppliedRoute.method && route.path === suppliedRoute.path){
+      return route.handler;
+    }
+    console.log("no route")    
+  }
+  return null;
+}
+
+
+var handleFormGet = (request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile('templates/form.html', 'utf8', function (err, data) {
       if (err) { throw err; }
       response.write(data);
       response.end();
     });
-  };
+  }
 
-server.on('request', function(request, response) {
- response.writeHead(200);
- response.write("<h1>Hello world!</h1>");
- response.end();
+var handleFormPost = (request, response) => {
+
+  response.writeHead(200, { "Content-Type": "text/html"});
+
+  var bodyString = '';
+  request.on('data', function(data){
+    console.log(`Data ${data}`)
+    bodyString += data;
+    console.log(`Body: ${bodyString}`)
+
+  });
+
+  request.on('end', function (){
+    console.log(`END: ${bodyString}`)
+    var post = queryString.parse(bodyString);
+    response.writeHead(200, { "Content-Type": "text/html"});
+    fs.readFile('templates/contacts.html', 'utf8', (err, data) =>{
+      if (err) { throw err }
+      var values = {
+        username: post['username'],
+        lastname: post['lastname']
+      }
+      var compiled = template(data, values)
+      response.write(compiled);
+      response.end();
+    })
+  });
+}
+
+server.on('request', function(request, response){
+  var handler = simpleRouter(request)
+
+  if (handler != null ){
+    handler(request, response)
+  }
+  else {
+    response.writeHead(404);
+    response.end();
+  }
+
 });
+
 
   server.listen(8888, function(){
       console.log('Listening on port 8888...')
