2722dd1cec2b66463ec29a643432d1f4aa15bca3
Lara Cook <190218@virtualwindow.co.za>
2020-06-10 15:16:39 +0200

    create a login page which allows a user to log in and go to the dashboard page, and take the user to a 404 if they do not log in correctly

--- index.js
+++ index.js
@@ -1,150 +1,225 @@
+const http = require("http");
+const queryString = require("querystring");
+const server = http.createServer();
 
-const http = require('http')
-const queryString = require('querystring')
-var fs =  require('fs')
-const template = require('es6-template-strings')
-const server = http.createServer()
-var contacts = [];
+var fs = require("fs");
+const template = require("es6-template-strings");
+var loggedInUsers = [];
 
+var databaseOfUser = [
+  {
+    username: "Lara",
+    password: "123",
+  },
+];
 
+//
+//
+//
+//Socket io
+var io = require("socket.io")(server);
 
+io.on("connection", (socket) => {
+  console.log("a user connected"),
+    io.emit("clientConnected", "Hello, you have connected to the chat server!");
 
-
-var io = require('socket.io').listen(server)
-
-io.on('connection', socket => {
-  console.log('a user connected'),
-  socket.emit('chat-message', 'Hello, you have connected to the chat server!')
-
-
-  socket.on('chatroom', function(msg){
-    console.log(`This is the message that was sent ${msg}`)
-    var emoji = "&#128540";
-    var res = msg.replace(":)", emoji)
-    io.emit('chatroom', res);
+  socket.on("chatroom", function (message) {
+    console.log(`This is the message that was sent ${message}`);
+    io.emit("chatroom", message);
   });
 
-  socket.on('broadcast', function(msg){
-    console.log(`This was the message that was sent ${msg}`)
+  /*socket.on('broadcast', function(message){
+    console.log(`This was the message that was sent ${message}`)
     socket.broadcast.emit('broadcast', 'Thank you for your message');
-  })
-
+  }) */
 });
 
-
-
-
-
 var simpleRouter = (request) => {
-  var method = request.method; 
+  var method = request.method;
   var path = request.url;
 
-
   //Strip the query from the ? character
-  var queryIndex = request.url.indexOf('?');
-  if (queryIndex >= 0){
-    path = request.url.slice(0, queryIndex)
+  var queryIndex = request.url.indexOf("?");
+  if (queryIndex >= 0) {
+    path = request.url.slice(0, queryIndex);
   }
 
-
-  var suppliedRoute = {method: method, path: path}
+  var suppliedRoute = { method: method, path: path };
   var routes = [
-    {method: 'GET', path: '/', handler: handleFormGet},
-    {method: 'GET', path: '/socket.io.js', handler: handleSocketGet},
-    {method: 'POST', path: '/', handler: handleFormPost}
+    { method: "GET", path: "/", handler: handleFormGet },
+    { method: "GET", path: "/login", handler: handleLoginGet },
+    { method: "POST", path: "/login", handler: handleLoginPost },
+    { method: "GET", path: "/socket.io.js", handler: handleSocketGet },
+    { method: "POST", path: "/", handler: handleFormPost },
   ];
 
-
-
-
-
   //Match the supplied route with the route visited
-  for (let i = 0; i < routes.length; i++){
-    var route = routes[i]
-    if(route.method === suppliedRoute.method && route.path === suppliedRoute.path){
+  for (let i = 0; i < routes.length; i++) {
+    var route = routes[i];
+    if (
+      route.method === suppliedRoute.method &&
+      route.path === suppliedRoute.path
+    ) {
       return route.handler;
     }
-    console.log("no route")    
+    console.log("no route");
   }
   return null;
-}
+};
 
-
-
-
-
-//Function to handle GET requests
+//
+//
+//
+//Functions to handle GET requests
 var handleFormGet = (request, response) => {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile('templates/form.html', 'utf8', function (err, data) {
-      if (err) { throw err; }
-      response.write(data);
-      response.end();
-    });
-  }
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("templates/login.html", "utf8", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    response.write(data);
+    response.end();
+  });
+};
 
+var handleSocketGet = function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("js/socket.js", "utf8", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    response.write(data);
+    response.end();
+  });
+};
 
-  var handleSocketGet = function(request, response){
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('js/socket.js', 'utf8', function(err, data){
-      if (err) { throw err; }
-      response.write(data);
-      response.end();
-    })
-  }
-
-
-
-
-
-//Function to handle POST requests
+//
+//
+//
+//Functions to handle POST requests
 var handleFormPost = (request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html"});
-  
-  var bodyString = '';
-  request.on('data', function(data){
+  response.writeHead(200, { "Content-Type": "text/html" });
+
+  var bodyString = "";
+  request.on("data", function (data) {
     //console.log(`Data ${data}`)
     bodyString += data;
     //console.log(`Body: ${bodyString}`)
-
   });
 
-  request.on('end', function (){
+  request.on("end", function () {
     //console.log(`END: ${bodyString}`)
     var post = queryString.parse(bodyString);
-    response.writeHead(200, { "Content-Type": "text/html"});
-    fs.readFile('templates/contacts.html', 'utf8', (err, data) =>{
-      if (err) { throw err }
-      var values = {
-        username: post['username'],
-        lastname: post['lastname']
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("templates/contacts.html", "utf8", (err, data) => {
+      if (err) {
+        throw err;
       }
-      var compiled = template(data, values)
+      var values = {
+        username: post["username"],
+      };
+      var compiled = template(data, values);
       response.write(compiled);
       response.end();
-    })
+    });
   });
-}
+};
 
+//
+//
+//
+// Login Handle Functions
+var handleLoginGet = (request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("templates/login.html", "utf8", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    response.write(data);
+    response.end();
+  });
+};
 
+var handleLoginPost = (request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
 
+  var bodyString = "";
+  request.on("data", (data) => {
+    console.log(`Data ${data}`);
+    bodyString += data;
+    //console.log(`Body: ${bodyString}`);
+  });
 
+  request.on("end", function () {
+    //console.log(`END: ${bodyString}`)
+    var post = queryString.parse(bodyString);
 
-//Call the 'on' method on the server 
-server.on('request', function(request, response){
+    var foundUser = false;
+
+    for (dbUser of databaseOfUser) {
+      if (
+        dbUser.username === post.username &&
+        dbUser.password === post.password
+      ) {
+        foundUser = true;
+        loggedInUsers.push(post);
+        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("templates/dashboard.html", "utf8", (err, data) => {
+          if (err) {
+            throw err;
+          }
+          var values = {
+            username: post["username"],
+            loggedUser: listLoginUsers(loggedInUsers),
+          };
+          var compiled = template(data, values);
+          response.write(compiled);
+          response.end();
+        });
+      }
+    }
+    if (!foundUser) {
+      response.writeHead(404, { "Content-Type": "text/html" });
+      fs.readFile("templates/404.html", "utf8", (err, data) => {
+        if (err) {
+          throw err;
+        }
+        var values = {
+          username: post["username"],
+        };
+        var compiled = template(data, values);
+        response.write(compiled);
+        response.end();
+      });
+    }
+  });
+};
+
+const listLoginUsers = (loggedUser) => {
+  var list = "";
+  for (user of loggedUser) {
+    list += `<li>
+            <h4>${user.username}</h4>
+          </li>`;
+  }
+  return list;
+};
+
+//
+//
+//
+//Call the 'on' method on the server
+server.on("request", function (request, response) {
   var handler = simpleRouter(request);
 
-  if (handler != null ){
-    handler(request, response)
-  }
-  else {
+  if (handler != null) {
+    handler(request, response);
+  } else {
     response.writeHead(404);
     response.end();
   }
-
 });
 
-
-  server.listen(8888, function(){
-      console.log('Listening on port 8888...')
-  });
+server.listen(8888, function () {
+  console.log("Listening on port 8888...");
+});
